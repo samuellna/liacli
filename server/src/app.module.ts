513f22e8bcd27363d2de24939e1a7e9387b59f18
samuellna <sna2@cin.ufpe.br>
2026-04-14 21:27:28 -0300

    feat(researchers): set up app module with ResearchersModule and TypeOrm configuration

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ResearchersModule } from './researchers/researchers.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    ResearchersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
