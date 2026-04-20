0c988acf968c0a597d5dabe2c92d334503416b02
samuellna <sna2@cin.ufpe.br>
2026-04-20 18:22:35 -0300

    feat(exams): implement runSeed function to initialize database with exam types, researchers, employees, samples, and sample results

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DataSource } from 'typeorm';
+import { runSeed } from './seed/seed';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const dataSource = app.get(DataSource);
+  await runSeed(dataSource);
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
