077bc7856cdffb8d3e73c05435d079c3edf14d9e
samuellna <sna2@cin.ufpe.br>
2026-06-08 12:35:38 -0300

    feat: implement AppController for dashboard data retrieval and add finalized projects endpoint in ResearchProjectsController

--- server/src/samples/samples.module.ts
+++ server/src/samples/samples.module.ts
@@ -8,13 +8,16 @@ import { EmployeesModule } from 'src/employees/employees.module';
 import { EmailModule } from 'src/email/email.module';
 import { ResearchProject } from 'src/researcher_projects/researcher_projects.entity';
 import { ResearchProjectsModule } from 'src/researcher_projects/researcher_projects.module';
+import { ResearchersModule } from 'src/researchers/researchers.module';
+import { Researchers } from 'src/researchers/researchers.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Sample, Employees, ResearchProject]),
+    TypeOrmModule.forFeature([Sample, Employees, ResearchProject, Researchers]),
     EmployeesModule,
     EmailModule,
     ResearchProjectsModule,
+    ResearchersModule,
   ],
   providers: [SamplesService],
   controllers: [SamplesController],
